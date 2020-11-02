0ca458566a22b0302220169c96eef9191d0f3692
GaGoRa <48663167+GaGoRa@users.noreply.github.com>
2020-11-02 18:50:40 -0300

    delete

--- README.md
+++
@@ -1,2 +0,0 @@
-# APICRUD-Mysql-Express-Nodejs
-This project is focused on creating a basic CRUD API. using principal Mysql , node js , Express, also several lib of third library for connect with database and validators.
--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-const indexController = require('../controller/controller-index')
-
-/* GET home page. */
-router.get('/', indexController.get);
-
-module.exports = router;
